99464d369682bb7664d183c517260a5b107ec932
Park Sungbum <106377447+multitab2022@users.noreply.github.com>
2024-02-01 00:01:15 +0900

    Create ubuntu2004.sh

---
+++ distro-plugins/ubuntu2004.sh
@@ -0,0 +1,5 @@
+DISTRO_NAME="Ubuntu (focal)"
+
+TARBALL_STRIP_OPT=0
+TARBALL_URL['aarch64']="https://cdimage.ubuntu.com/ubuntu-base/releases//focal/release/ubuntu-base-20.04.5-base-arm64.tar.gz"
+TARBALL_SHA256['aarch64']="f9b999afb4c4b10193087ea8c11be36d688f19e609b05179b571f29357954b52"
