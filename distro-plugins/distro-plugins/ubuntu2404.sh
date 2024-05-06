0f8739a40e1eed437131b96a9142fa2b243ec22f
Park Sungbum <106377447+multitab2022@users.noreply.github.com>
2024-05-06 22:45:35 +0900

    Create ubuntu2404.sh

---
+++ distro-plugins/distro-plugins/ubuntu2404.sh
@@ -0,0 +1,5 @@
+DISTRO_NAME="Ubuntu (Noble)"
+
+TARBALL_STRIP_OPT=0
+TARBALL_URL['aarch64']="https://cdimage.ubuntu.com/ubuntu-base/releases/24.04/release/ubuntu-base-24.04-base-arm64.tar.gz"
+TARBALL_SHA256['aarch64']="97a9c56f60e34ebda3a7d803a6dbcf23e1c4b366bcf37b6bf7cee2933c3f553c"
